86d84b1ab172da08d5d62d8513360f78b84a4b64
sadeheden <sadeheden@gmail.com>
2024-08-29 21:06:20 +0300

    TRYING TO MAKE NICE MOBILE

--- script/banner.js
+++ script/banner.js
@@ -142,3 +142,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     saveButton.addEventListener("click", saveBanner);
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('navLinks');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
