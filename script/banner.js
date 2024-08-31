48b1d714c4bbc95c19aadf8378ea3798f8a24958
sadeheden <sadeheden@gmail.com>
2024-08-31 16:36:49 +0300

    .

--- script/banner.js
+++ script/banner.js
@@ -142,11 +142,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
     saveButton.addEventListener("click", saveBanner);
 });
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('navLinks');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
